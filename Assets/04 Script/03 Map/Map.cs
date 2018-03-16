74b075f0bad586de218aed1ff5270fd1ba44d469
plasma0712 <plasma0712@naver.com>
2018-03-16 18:06:25 +0900

    1. XML에  각 타일별 Type을 저장완료.
    2. XML에 저장된 Type을 통하여 타일생성.
    
    앞으로 해야 할 일.
    
    1. 몬스터 생성방식 생각하기( 드래그로 끄는것이 가장 무난할 것이라 생각함)
    
    2. 몬스터 생성을 하기(몬스터의 정보는 XML로 관리, 몬스터 생성시 XML로 위치값과 몬스터고유이름 저장하기)

--- Assets/04 Script/03 Map/Map.cs
+++ Assets/04 Script/03 Map/Map.cs
@@ -7,16 +7,22 @@ public class Map : Singleton<Map>
 {
     float TileX = 0.62f;
     float TileY = 0.62f;
-
+    [HideInInspector]
     public int TileXData = 0;
+    [HideInInspector]
     public int TileYData = 0;
+    [HideInInspector]
     public int MapCount;
+    [HideInInspector]
     public int MapSize;
 
+    float tileTypeData;
+
+    float TileType;
     XMLMapData Current;
     Vector3 vPos;
-    public GameObject Tile;
-    public GameObject Parent;
+    //public GameObject Tile;
+    public GameObject Parent;                               // 맵 타일 
     [SerializeField]
     List<GameObject> TileList = new List<GameObject>();
 
@@ -31,9 +37,10 @@ public class Map : Singleton<Map>
         Current = XMLMap.Instance.GetMapData(_mapdata);
         TileXData = Current.iMapTileX;
         TileYData = Current.iMapTileY;
+        tileTypeData = Current.fType;
         vPos.x = TileX * TileXData;
         vPos.y = TileY * TileYData;
-
+       
         //Debug.Log("X_Pos : " + TileXData + "Y_Pos : " + TileYData);
     }
 
@@ -44,7 +51,7 @@ public class Map : Singleton<Map>
         for (int k = 0; k < XMLMap.Instance.MapLength(); k++)
         {
             CurrentMapData(k);
-            GameObject instance = (GameObject)Instantiate(Tile, vPos, Quaternion.identity);
+            GameObject instance = (GameObject)Instantiate(TileInfo.Instance.TileType[(int)tileTypeData], vPos, Quaternion.identity);
             instance.transform.parent = Parent.transform; // 인스턴트로 생성된 오브젝트를 정리하기 위해서 이용함.
             TileList.Add(instance);                       // 인스턴트로 생성된 오브젝트를 리스트로 넣어 관리하기 위해 함.
             MapCount++;                                   // MapCount를 이용하여 현재 깔린 타일의 수를 알기 위함
@@ -58,7 +65,138 @@ public class Map : Singleton<Map>
         {
             for (int TileX = 0; TileX < 7; TileX++)
             {
-                XMLMap.Instance.AddXmlNode(((TileY * 7 + TileX) + MapCount).ToString(), (TileX + 7 * MapSize).ToString(), TileY.ToString(), "0");
+                switch (TileY)
+                {
+                    case 0:
+                        if (TileX == 0)
+                        {
+                            TileType = 1;
+                        }
+                        else if (TileX == 6)
+                        {
+                            TileType = 4;
+                        }
+                        else
+                        {
+                            TileType = 8;
+                        }
+                        break;
+
+                    case 1:
+                        if (TileX == 0)
+                        {
+                            TileType = 5;
+                        }
+                        else if (TileX == 6)
+                        {
+                            TileType = 7;
+                        }
+                        else
+                        {
+                            TileType = 0;
+                        }
+                        break;
+
+                    case 2:
+                        if (TileX == 0)
+                        {
+                            TileType = 5;
+                        }
+                        else if (TileX == 6)
+                        {
+                            TileType = 7;
+                        }
+                        else
+                        {
+                            TileType = 0;
+                        }
+                        break;
+
+                    case 3:
+                        if (TileX == 0)
+                        {
+                            TileType = 5;
+                        }
+                        else if (TileX == 6)
+                        {
+                            TileType = 7;
+                        }
+                        else
+                        {
+                            TileType = 0;
+                        }
+                        break;
+
+                    case 4:
+                        TileType = 0;
+                        break;
+
+                    case 5:
+                        if (TileX == 0)
+                        {
+                            TileType = 5;
+                        }
+                        else if (TileX == 6)
+                        {
+                            TileType = 7;
+                        }
+                        else
+                        {
+                            TileType = 0;
+                        }
+                        break;
+
+                    case 6:
+                        if (TileX == 0)
+                        {
+                            TileType = 5;
+                        }
+                        else if (TileX == 6)
+                        {
+                            TileType = 7;
+                        }
+                        else
+                        {
+                            TileType = 0;
+                        }
+                        break;
+
+                    case 7:
+                        if (TileX == 0)
+                        {
+                            TileType = 5;
+                        }
+                        else if (TileX == 6)
+                        {
+                            TileType = 7;
+                        }
+                        else
+                        {
+                            TileType = 0;
+                        }
+                        break;
+
+                    case 8:
+                        if (TileX == 0)
+                        {
+                            TileType = 2;
+                        }
+                        else if (TileX == 6)
+                        {
+                            TileType = 3;
+                        }
+                        else
+                        {
+                            TileType = 6;
+                        }
+
+                        break;
+
+                    default:
+                        break;
+
+                }
+                XMLMap.Instance.AddXmlNode(((TileY * 7 + TileX) + MapCount).ToString(), (TileX + 7 * MapSize).ToString(), TileY.ToString(), TileType.ToString());
             }
         }
 
--- Assets/04 Script/03 Map/TileInfo.cs
+++ Assets/04 Script/03 Map/TileInfo.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TileInfo : MonoBehaviour
+public class TileInfo : Singleton<TileInfo>
 {
     public GameObject []TileType;
     

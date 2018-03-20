ad5127cdcc63d85c698747b3b36e615c7ec6cab5
plasma0712 <plasma0712@naver.com>
2018-03-20 20:19:00 +0900

    --

---
+++ Assets/04 Script/03 Map/MouseSeletionPoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseSeletionPoint : MonoBehaviour
+{
+    public GameObject followingCube;
+
+    void Update()
+    {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z + 5));
+        followingCube.transform.position = newPosition;
+    }
+
+}
